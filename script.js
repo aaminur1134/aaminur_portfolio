38696417d1a6368fb72c0e76e8fddac53ae59d30
aaminur__ <71035151+aaminur1134@users.noreply.github.com>
2021-11-03 23:52:09 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows : false,
+        dots : true,
+        appendDots: '.slider-dots',
+        dotsClass: 'dots',
+        autoplay: true,
+        autoplaySpeed: 3000,
+
+    });
+
+});
